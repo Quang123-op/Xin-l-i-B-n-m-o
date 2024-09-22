5e735ea266cd55b0e7dad9b2e39dfbfd547d6eec
Quang123-op <duyqqq3334@gmail.com>
2024-09-22 21:41:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại là con mèo dthw v nhất ta <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Tha thì tha không ta cũng phải tha <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn mèo dthw nhiều nhiều 😘😘',
+    mess: 'Mình biết mà 🥰. Yêu bạn mèo dthw nhiều nhiều😘😘',
     messDesc: 'mai, mình mua đồ ăn cho nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=61550832330322' //link mess của các bạn. VD: /dinhkaito
